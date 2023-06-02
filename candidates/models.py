d89866e904e619b0e2f59a7472df9411844dbf16
Pakshal Ghiya <pakshal@simformsolutions.com>
2023-06-02 19:39:13 +0530

    Initial Commits

---
+++ candidates/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User 
+
+# Create your models here.
+# class candidate(models.Model):
+#     user= models.OneToOneField(User,verbose_name=,blank=, null=)
+#     full_Name=
+#     skills=
+#     choice(Type)=
+#     Resume=
+#     profile_photo=
+#     Experience(In Years)=
+#     Graduation_Year=
+#     Country=
+#     Location(Address)=
+#     Address=
+#     Portfolio_Link=
+#     Bio(Additional Details)=
